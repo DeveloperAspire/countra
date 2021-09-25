b2388207504b1d4d041b9d92aa65450c56ba3de1
Franklin <franklinokolie86@gmail.com>
2021-09-25 14:22:16 +0100

    Added Hashmode for routing and added responsivness

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./store/index";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
